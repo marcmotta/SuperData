2246e69910cb6198c6c31b8163daa86321ba84fe
Marcos Mota <60157172+marcmotta@users.noreply.github.com>
2025-07-27 11:02:24 +0700

    Add superdata.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/superdata/superdata.go
@@ -0,0 +1,36 @@
+// internal/superdata/superdata.go
+package superdata
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting SuperData processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
